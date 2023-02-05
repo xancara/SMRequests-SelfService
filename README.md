f25a679ddb74e45aff15368ba68937a62aabd9a0
xancara <xancara@gmail.com>
2023-02-04 21:00:57 -0500

    Resolving commit issues
    Resolving the issues from the earlier commit and PR revert issue.

--- README.md
+++ README.md
@@ -1,20 +1,28 @@
 # SMRequests-SelfService
 
- The login system and self service potral for SMRequests
+Login System, Automated User Provisioning, and Self-Service Management functionality for the SMRequests system.
+ 
+This repository is separate from the primary user-facing repository to ensure integrity of production systems that live-streamers rely on.
  
 # Contributions
 
-Known contributions will be from PennWest University 2022-2023 Senior CIS Capstone
+Known contributions will be from PennWest University 2022-2023 Senior CIS Capstone Team consisting of:
+
+J. Sayre - https://github.com/falseparallax/
+
+S. Dixon - https://github.com/dix4724/
+
+M. Seibel - https://github.com/seibelstudios/
 
 # System Maintainers
 
-MrTwinkles - https://github.com/MrTwinkles47
+MrTwinkles47 - https://github.com/MrTwinkles47
 
 xancara - https://github.com/xancara
 
 # Approvers
 
-MrTwinkles - https://github.com/MrTwinkles47
+MrTwinkles47 - https://github.com/MrTwinkles47
 
 xancara - https://github.com/xancara
 
@@ -24,7 +32,7 @@ DaveLinger - https://github.com/DaveLinger
 
 # Attribution
 
-Base code from Easy, Code Is system for a starting point
+Base code from Easy, Code Is system for a starting point for login and account management
 Link: https://github.com/jstolpe/easycodeis
 
 Used for SMRequests Streamer Tools
---
+++ index.php
@@ -0,0 +1,160 @@
+<!--
+* Module Name: 	Index.php
+* Date: 		2023-02-04
+* Author:		J. Sayre
+				Adapted from Easy, Code Is by Jstolpe Repository: https://github.com/jstolpe/easycodeis per request from maintainers.
+* Purpose:		Post-authentication landing page and navigation.
+-->
+<?php
+	// Load global resources and establish a session
+	include_once 'autoloader.php';
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- title of our page -->
+		<title>SMRequests Development | WIP</title>
+
+		<!-- include fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Coda" rel="stylesheet">
+
+		<!-- mobile layout support -->
+		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+
+		<!-- css styles for our home page-->
+		<link href="css/global.css" rel="stylesheet" type="text/css">
+		<link href="css/home.css" rel="stylesheet" type="text/css">
+
+		<!-- jquery -->
+		<script type="text/javascript" src="js/jquery.js"></script>
+
+		<!-- include our loader overlay script -->
+		<script type="text/javascript" src="js/loader.js"></script>
+
+		<script>
+			$( function() { // do things when the document is ready
+				// initialize our loader overlay
+				loader.initialize();
+
+				$( '#load_test' ).on( 'click', function() { // on click for our load test link
+					// show our loading overlay
+					loader.showLoader();
+
+					setInterval( function() { // after 3 seconds, hide our loading overlay
+						loader.hideLoader();
+					}, 3000 );
+				} );
+
+				$( '#logout_link' ).on( 'click', function() { // on click for our logout link
+					// show our loading overlay
+					loader.showLoader();
+
+					// server side logout
+					$.ajax( {
+						url: 'php/process_logout.php',
+						type: 'post',
+						dataType: 'json',
+						success: function( data ) {
+							loader.hideLoader();
+							window.location.href = "index.php";
+						}
+					} );
+				} );
+			} );
+		</script>
+	</head>
+	<body>
+		<!--Low-Priority Request from Maintainers: Replace Video with some other asset-->
+		<div class="background-video-container">
+			<video class="background-video-element" autoplay muted loop >
+				<source src="assets/background_video.mp4" />
+			</video>
+			<img class="background-video-image" src="assets/background_video_image.png" />
+			<div class="background-video-overlay"></div>
+			<div class="background-video-text-overlay">
+				<div>SMRequests Development | Work In Progress</div>
+				<!--Desktop Client Experience-->
+				<div class="action-container pc-only">
+					<?php if ( isLoggedIn() ) : ?> <!--Check for logged-in user and greet-->
+						<div class="logged-in-text">Logged in as <b><?php echo $_SESSION['user_info']['first_name']; ?></b></div>
+					<?php else : ?> <!--Display buttons-->
+						<a class="a-action" href="signup.php">
+							<div class="button-container">
+								<div class="button-container-pad">
+									SIGN UP
+								</div>
+							</div>
+						</a>
+						<a class="a-action" href="login.php">
+							<div class="button-container">
+								<div class="button-container-pad">
+									LOGIN
+								</div>
+							</div>
+						</a>
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
+		<div class="content">
+			<div class="content-inner">
+				<div class="content-inner-padding">
+					<!--Mobile Experience-->
+					<div class="action-container mobile-only">
+						<?php if ( isLoggedIn() ) : ?> <!--Check for logged-in user and greet-->
+							<div class="logged-in-text">Logged in as <b><?php echo $_SESSION['user_info']['first_name']; ?></b></div>
+						<?php else : ?> <!--Display buttons-->
+							<a class="a-action" href="signup.php">
+								<div class="button-container">
+									<div class="button-container-pad">
+										SIGN UP
+									</div>
+								</div>
+							</a>
+							<a class="a-action" href="login.php">
+								<div class="button-container default-margin-top">
+									<div class="button-container-pad">
+										LOGIN
+									</div>
+								</div>
+							</a>
+						<?php endif; ?>
+					</div>
+					<h1>
+						Welcome to SMRequests!
+					</h1>
+					<div>
+						This is a skeleton of the main site that is based off of the open source code listed in the github link below. <br/>It has been modified to reflect all necessary functions for SMRequests. Development is just stating on this so hang in there with us while we work through things.
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer-container">
+			<div><a class="a-default" href="https://github.com/MrTwinkles47/Stepmania-Stream-Tools-MrTwinkles">View SMRequests on GitHub</a></div>
+			<?php if ( isLoggedIn() ) : ?>
+				<?php if ( isAdmin() ) : ?>
+					<div>
+						<a class="a-default" href="adminpanel.php">Admin Panel</a>
+					</div>
+				<?php endif; ?>
+				<div>
+					<a class="a-default" href="myaccount.php">My Account</a>
+				</div>
+				<div>
+					<a class="a-default" href="mysettings.php">Manage Settings</a>
+				</div>
+				<div>
+					<a class="a-default" href="myviewers.php">Manage Requestors</a>
+				</div>
+				<div>
+					<a class="a-default" href="mysongs.php">Manage Songs</a>
+				</div>
+				<div>
+					<a class="a-default" href="mywebhooks.php">Manage Webhooks</a>
+				</div>
+				<div id="logout_link" class="a-default">Logout</div>
+			<?php endif; ?>
+			<div><a class="a-default" href="https://github.com/jstolpe/easycodeis">View Easy, Code Is on GitHub</a></div>
+		</div>
+	</body>
+</html>
