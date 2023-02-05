cac12d67c37adc402d39ffb18f7978c32a785286
phixious <xancara@users.noreply.github.com>
2023-02-04 19:52:11 -0500

    Revert "Comments & Work Setup for Capstone Team"

--- login.php
+++ login.php
@@ -1,72 +1,59 @@
-<!--
-* Module Name: 	Login.php
-* Date: 		2023-01-25
-* Author:		J. Sayre
-				Adapted from Easy, Code Is by Jstolpe Repository: https://github.com/jstolpe/easycodeis per request from maintainers.
-* Purpose:		Enables OAuth login to Twitch.tv as well as future-state username/password authentication.
-				Support for Facebook and Twitter login is also present, but will not be accessible to users
-				via styling which hides the user interaction points (buttons) with the initial release of 
-				this module as the SMRequests platform does not yet support these.
-* Notes:		
--->
 <?php
-	// Creates a Session, Loads global APIs and other global resources
+	// load up global things
 	include_once 'autoloader.php';
 
-	// Facebook - Note that SMR users will not be logging in with this method at initial implementation.
 	if ( isset( $_GET['state'] ) && FB_APP_STATE == $_GET['state'] ) { // coming from facebook
 		// try and log the user in with $_GET vars from facebook 
 		$fbLogin = tryAndLoginWithFacebook( $_GET );
 	}
 
-	// Twitter = Note that SMR users will not be logging in with this method at initial implementation.
-	if ( isset( $_GET['oauth_token'] ) && isset( $_GET['oauth_verifier'] ) && isset( $_SESSION['request_oauth_token'] ) && $_SESSION['request_oauth_token'] == $_GET['oauth_token'] ) { // coming from Twitter and not authorized
-		// Twitter Login: Create a new instance of the Twitter API, get Twitter login url
+	if ( isset( $_GET['oauth_token'] ) && isset( $_GET['oauth_verifier'] ) && isset( $_SESSION['request_oauth_token'] ) && $_SESSION['request_oauth_token'] == $_GET['oauth_token'] ) { // coming from twitter and not authorized
+		// twitter login data, instantiate new twitter api and get twitter login url
 		$eciTwitterApi = new eciTwitterApi( TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, $_SESSION['request_oauth_token'], $_SESSION['request_oauth_token_secret'] );
 		
-		// get Twitter access token
+		// get access token
 		$twitterAccessToken = $eciTwitterApi->getAccessToken( $_GET['oauth_verifier'] );
 
-		// set Twitter session
+		// set session
 		$_SESSION['oauth_token'] = !empty( $twitterAccessToken['api_data']['oauth_token'] ) ?  $twitterAccessToken['api_data']['oauth_token'] : '';		
 		$_SESSION['oauth_token_secret'] = !empty( $twitterAccessToken['api_data']['oauth_token_secret'] ) ? $twitterAccessToken['api_data']['oauth_token_secret'] : '';
 
-		// Twitter Login: Create a new instance of the Twitter API, get Twitter login url
+		// twitter login data, instantiate new twitter api and get twitter login url
 		$eciTwitterApi = new eciTwitterApi( TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret'] );
 
 		// get user info
 		$twitterLogin = $eciTwitterApi->tryAndLoginWithTwitter();
 	}
 
-	// Twitter Login: Create a new instance of the Twitter API, get Twitter login url
+	// twitter login data, instantiate new twitter api and get twitter login url
 	$eciTwitterApi = new eciTwitterApi( TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET );
 	$twitterPreLoginData = $eciTwitterApi->getDataForLogin( TWITTER_CALLBACK_URL );
 
 	if ( isset( $_GET['code'] ) && isset( $_GET['state'] ) && $_GET['state'] == $_SESSION['twitch_state'] ) { // user is coming from twitch
-		// Create a new instance of the Twitch API
+		// instantiate new twitch class
 		$eciTwitchApi = new eciTwitchApi( TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET );
 
-		// Attempt Twitch Login
+		// try and log the user in with twitch
 		$twitchLogin = $eciTwitchApi->tryAndLoginWithTwitch( $_GET['code'], TWITCH_REDIRECT_URI );
 	}
 
-	// get Twitch login url
+	// get twitch login url
 	$eciTwitchApi = new eciTwitchApi( TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET );
 	$twitchLoginUrl = $eciTwitchApi->getLoginUrl( TWITCH_REDIRECT_URI );
 
-	// Prevent logged-in users from viewing the login page
+	// only if you are logged out can you view the login page
 	loggedInRedirect();
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<!-- page title -->
-		<title>SMRequests Development Site | Login</title>
+		<!-- title of our page -->
+		<title>Easy, Code Is | Login</title>
 
-		<!-- fonts -->
+		<!-- include fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Coda" rel="stylesheet">
 
-		<!-- mobile layout support -->
+		<!-- need this so everything looks good on mobile devices -->
 		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 
 		<!-- css styles for our login page-->
@@ -95,9 +82,8 @@
 				} );
 			} );
 
-			// Function to process login, including clearing of error styling.
 			function processLogin() {
-				// Clear error styling when a user chooses to sign up.
+				// clear error message and red borders on signup click
 				$( '#error_message' ).html( '' );
 				$( '#error_message_fb_php' ).html( '' );
 				$( '#error_message_twitter_php' ).html( '' );
@@ -133,7 +119,7 @@
 							}
 						}
 					} );
-				} else { // Incomplete fields, present message and scroll to top of page
+				} else { // some fields are not filled in, show error message and scroll to top of page
 					$( '#error_message' ).html( 'All fields must be filled in.' );
 					$( window ).scrollTop( 0 );
 				}
@@ -144,7 +130,7 @@
 		<div class="site-header">
 			<div class="site-header-pad">
 				<a class="header-home-link" href="index.php">
-					<span id="icon"></span>&nbsp;SMRequests
+					Easy, Code Is
 				</a>
 			</div>
 		</div>
@@ -154,55 +140,44 @@
 					<div class="site-content-section-inner">
 						<div class="section-heading">Login</div>
 						<form id="login_form" name="login_form">
-
-							<!-- Present errors to users if necessary -->
 							<div id="error_message" class="error-message">
-								<!--Facebook error-->
-								<?php if ( isset( $_SESSION['eci_login_required_to_connect_facebook'] ) && $_SESSION['eci_login_required_to_connect_facebook'] ) : // Facebook Password Entry ?>
+								<?php if ( isset( $_SESSION['eci_login_required_to_connect_facebook'] ) && $_SESSION['eci_login_required_to_connect_facebook'] ) : // enter password to connect account ?>
 									<div style="margin-bottom:10px;">
 										An account already exists with that email address. To connect your Facebook account, enter your password.
 									</div>
 								<?php endif; ?>
-
-								<!--Twitter error-->
-								<?php if ( isset( $_SESSION['eci_login_required_to_connect_twitter'] ) && $_SESSION['eci_login_required_to_connect_twitter'] ) : // Twitter Password Entry ?>
+								<?php if ( isset( $_SESSION['eci_login_required_to_connect_twitter'] ) && $_SESSION['eci_login_required_to_connect_twitter'] ) : // enter password to connect account ?>
 									<div style="margin-bottom:10px;">
 										An account already exists with that email address. To connect your Twitter account, enter your password.
 									</div>
 								<?php endif; ?>
-
-								<!--Twitch error-->
-								<?php if ( isset( $_SESSION['eci_login_required_to_connect_twitch'] ) && $_SESSION['eci_login_required_to_connect_twitch'] ) : // Twitch Password Entry ?>
+								<?php if ( isset( $_SESSION['eci_login_required_to_connect_twitch'] ) && $_SESSION['eci_login_required_to_connect_twitch'] ) : // enter password to connect account ?>
 									<div style="margin-bottom:10px;">
 										An account already exists with that email address. To connect your Twitch account, enter your password.
 									</div>
 								<?php endif; ?>
-
 							</div>
 							<div>
-								<!-- Email Field -->
 								<div class="section-label">Email</div>
 								<div>
-									<?php if ( isset( $_SESSION['fb_user_info']['email'] ) ? $_SESSION['fb_user_info']['email'] : '' ) : // Check if there's a Facebook user session ?>
-										<?php $inputEmail = $_SESSION['fb_user_info']['email']; // If so, set the email variable to the user's Facebook email ?>
-									<?php elseif ( isset( $_SESSION['tw_user_info']['email'] ) ? $_SESSION['tw_user_info']['email'] : '' ) : // Check if there's a Twitter user session ?>
-										<?php $inputEmail = $_SESSION['tw_user_info']['email']; // If so, set the email variable to the user's Twitter email ?>
-									<?php elseif ( isset( $_SESSION['twitch_user_info']['email'] ) && $_SESSION['twitch_user_info']['email'] ) : // Check if there's a Twitch user session ?>
-										<?php $inputEmail = $_SESSION['twitch_user_info']['email']; // If so, set the email variable to the user's Twitch email ?>
-									<?php else : // Otherwise,  ?>
-										<?php $inputEmail = ''; // Set the email variable to a blank  ?> 
-									<?php endif; ?> 
-									<input class="form-input" type="text" name="email" value="<?php echo $inputEmail; // Populate the email input with the previously-set email variable's value ?>" />
+									<?php if ( isset( $_SESSION['fb_user_info']['email'] ) ? $_SESSION['fb_user_info']['email'] : '' ) : // pre populate with facebook email ?>
+										<?php $inputEmail = $_SESSION['fb_user_info']['email']; ?>
+									<?php elseif ( isset( $_SESSION['tw_user_info']['email'] ) ? $_SESSION['tw_user_info']['email'] : '' ) : // pre populate with twitter email ?>
+										<?php $inputEmail = $_SESSION['tw_user_info']['email']; ?>
+									<?php elseif ( isset( $_SESSION['twitch_user_info']['email'] ) && $_SESSION['twitch_user_info']['email'] ) : ?>
+										<?php $inputEmail = $_SESSION['twitch_user_info']['email']; ?>
+									<?php else : ?>
+										<?php $inputEmail = ''; ?>
+									<?php endif; ?>
+									<input class="form-input" type="text" name="email" value="<?php echo $inputEmail; ?>" />
 								</div>
 							</div>
 							<div class="section-mid-container">
-								<!-- Password Field -->
 								<div class="section-label">Password</div>
 								<div><input class="form-input" type="password" name="password" /></div>
 							</div>
 						</form>
 						<div class="section-action-container">
-							<!-- Login Button -->
 							<div class="section-button-container" id="login_button">
 								<div>Login</div>
 							</div>
@@ -210,59 +185,45 @@
 						<div class="section-action-container">
 							- OR -
 						</div>
-						<!--	Facebook Error Message Container, styled so it is not visible to users.
-								We're leaving this code in place to allow for future-state support of 
-								Facebook-based login as Facebook is a streaming platform too. -->
-						<div class="section-action-container" style="display:none;">
-							<div id="error_message_fb_php" class="error-message" >
-								<?php if ( !empty( $fbLogin['status'] ) && 'fail' == $fbLogin['status'] ) : // display Facebook error ?>
+						<div class="section-action-container">
+							<div id="error_message_fb_php" class="error-message">
+								<?php if ( !empty( $fbLogin['status'] ) && 'fail' == $fbLogin['status'] ) : // we have a facebook error to display ?>
 									<?php echo $fbLogin['message']; ?>
 								<?php endif; ?>
 							</div>
 						</div>
-						<!--	Facebook Login Button Container, styled so it is not visible to users.
-								We're leaving this code in place to allow for future-state support of 
-								Facebook-based login as Facebook is a streaming platform too. -->
-						<div class="section-action-container" style="display:none;">
+						<div class="section-action-container">
 							<a href="<?php echo getFacebookLoginUrl(); ?>" class="a-fb">
 								<div class="fb-button-container">
 									Login with Facebook (PHP)
 								</div>
 							</a>
 						</div>
-						<!--	Twitter Error Message Container, styled so it is not visible to users.
-								We're leaving this code in place to allow for future-state support of 
-								Twitter-based login. -->
-						<div class="section-action-container" style="display:none;">
+						<div class="section-action-container">
 							<div id="error_message_twitter_php" class="error-message">
-								<?php if ( 'fail' == $twitterPreLoginData['status'] ) : // display Twitter error ?>
+								<?php if ( 'fail' == $twitterPreLoginData['status'] ) : // twitter fail ?>
 									<div>
 										<?php echo $twitterPreLoginData['message']; ?>
 									</div>
 								<?php endif; ?>
 							</div>
 						</div>
-						<!--	Twitter Login Button Container, styled so it is not visible to users.
-								We're leaving this code in place to allow for future-state support of 
-								Twitter-based login. -->
-						<div class="section-action-container" style="display:none;">
+						<div class="section-action-container">
 							<a href="<?php echo $twitterPreLoginData['twitter_login_url'] ;?>" class="a-tw">
 								<div class="tw-button-container">
 									Login with Twitter (PHP)
 								</div>
 							</a>
 						</div>
-						<!--	Twitch Error Message - Visible to Users on unsuccessful login attempts -->
 						<div class="section-action-container">
 							<div id="error_message_twitch_php" class="error-message">
-								<?php if ( isset( $twitchLogin['status'] ) && 'fail' == $twitchLogin['status'] ) : // display Twitch error ?>
+								<?php if ( isset( $twitchLogin['status'] ) && 'fail' == $twitchLogin['status'] ) : ?>
 									<div>
 										<?php echo $twitchLogin['message']; ?>	
 									</div>
 								<?php endif; ?>
 							</div>
 						</div>
-						<!-- Twitch Login Button. Most users will login via this way. -->
 						<div class="section-action-container">
 							<a href="<?php echo $twitchLoginUrl; ?>" class="a-twitch">
 								<div class="twitch-button-container">
@@ -270,7 +231,6 @@
 								</div>
 							</a>
 						</div>
-						<!-- Sign-Up Link -->
 						<div class="section-footer-container">
 							Not a member? <a class="a-default" href="signup.php">Sign Up</a>
 						</div>
