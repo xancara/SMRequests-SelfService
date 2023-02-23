b1d0f38ab4868f041233c97fbc41bdec7ab8aa0c
Jenn S <112665962+falseparallax@users.noreply.github.com>
2023-02-23 14:46:13 -0500

    Remove duplicate function wh_log

--- php/functions.php
+++ php/functions.php
@@ -18,19 +18,6 @@
 		}
 	}
 
-	function wh_log($log_msg){
-		$log_filename = __DIR__."/log";
-		if (!file_exists($log_filename)) 
-		{
-			// create directory/folder uploads.
-			mkdir($log_filename, 0777, true);
-		}
-		$log_file_data = $log_filename.'/log_' . date('Y-m-d') . '.log';
-		$log_msg = rtrim($log_msg); //remove line endings
-		// if you don't add `FILE_APPEND`, the file will be erased each time you add a log
-		file_put_contents($log_file_data, date("Y-m-d H:i:s") . " -- [" . strtoupper(basename(__FILE__)) . "] : ". $log_msg . PHP_EOL, FILE_APPEND);
-	}
-	
 	/**
 	 * Get SMR User DB connection 
 	 *
