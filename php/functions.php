f2659337f4e8a7319ec986cf9a5ced45b6200b5d
Jenn S <112665962+falseparallax@users.noreply.github.com>
2023-02-23 15:28:09 -0500

    Realign with master
    Fixing commits from upstream issues.

--- php/functions.php
+++ php/functions.php
@@ -58,45 +58,6 @@
 	}
 	
 	/**
-	 * Get SMR User DB connection 
-	 *
-	 * @param smrUser
-	 *
-	 * @return db connection
-	 */
-	function getSMRDatabaseConnection( $smrUser ) {
-		try { // connect to database and return connections
-			//wh_log("In Get Database Connection" . PHP_EOL);
-			//wh_log("DB INfo is - Host: " . DB_HOST . ", Name: " . DB_NAME . " , User: " . DB_USER . ", Pass: " . DB_PASS . " right now" . PHP_EOL);
-			$conn = new PDO( 'mysql:host=' . DB_HOST . ';dbname=' . DB_PREFIX . $smrUser , DB_USER, DB_PASS );
-			//wh_log("Connection is " . $conn . " right now" . PHP_EOL);
-			return $conn;
-		} catch ( PDOException $e ) { // connection to database failed, report error message
-			return $e->getMessage();
-		}
-	}
-
-	/**
-	 * Post message to Log
-	 *
-	 * @param message
-	 *
-	 * @return void
-	 */
-	function wh_log($log_msg){
-		$log_filename = __DIR__."/log";
-		if (!file_exists($log_filename)) 
-		{
-			// create directory/folder uploads.
-			mkdir($log_filename, 0777, true);
-		}
-		$log_file_data = $log_filename.'/log_' . date('Y-m-d') . '.log';
-		$log_msg = rtrim($log_msg); //remove line endings
-		// if you don't add `FILE_APPEND`, the file will be erased each time you add a log
-		file_put_contents($log_file_data, date("Y-m-d H:i:s") . " -- [" . strtoupper(basename(__FILE__)) . "] : ". $log_msg . PHP_EOL, FILE_APPEND);
-	}
-	
-	/**
 	 * Update user
 	 *
 	 * @param array $info
