16d69fedf98ea4c0d19014fbf52f1b5a1491c665
Jenn S <112665962+falseparallax@users.noreply.github.com>
2023-03-29 12:31:42 -0400

    Corrections to Settings (#15)
    * Login Functionality
    
    Customization of the Easy, Code Is template to enable Twitch OAuth. Notably, login functions for Facebook and Twitter as disabled; although SMR may support these methods in the future, right now the scope will be limited to Twitch.
    
    * Addition of Comments
    
    Elaborated and refined comments in login.php and index.php.
    
    Added top code blocks to the modules  which align with our scope -  for authoring, module purpose, and notes for update by team members as they work through the project.
    
    These pages have existing .php files, but are just templates/copies from another page, and will need to be heavily revamped to implement the necessary functionality. The thought here was to provide a consistent starting point for the team.
    
    * Roll Back / Comment Update
    
    Previous commit had an error that was preventing successful execution of isLoggedIn() and, subsequently, loggedInRidirect(). Additionally implemented feedback that previous comments were too verbose; pared them back. Tested locally.
    
    * Address Comment Block Issue
    
    Discovered that although the previous commit tested successfully locally, it did not work when hosted on a webserver.
    
    Issue resulted from the comment blocks at the top, which were formatted as HTML comments. As it turns out, a PHP session cannot start after ANY HTML has been executed - even if it is just a comment - in a non-local setting. Resolved and retested.
    
    * Removal of hosting site files
    
    Remove unnecessary files from hosting service.
    
    * Revert "Comments & Work Setup for Capstone Team"
    
    * Revert "Merge branch 'master' into pr/3"
    
    This reverts commit f93d7134860cf94d3ccc996a64e6b4e32d947154, reversing
    changes made to deb67dab8c1db683f0bf19d828ee7f7d6c6b7404.
    
    * Commit after Rebase
    
    Resolving prior PR/merge conflict issues caused by my user-error.
    
    * Persistent & Dynamic Navigation
    
    Changes primarily add functionality to dynamically display navigation options to users based on their user level.
    Additionally, to streamline future-state updates, the navigation has been shifted to its own file and included on all pages within scope. This allows for persistent navigation without having to have nav & logic on each page.
    
    Finally, revisited some of the page templates and cleaned them up by both renaming unique functions and slimming down some of the bulk that won't be relevant for reference use. Created related process_my<file>.php files where they'll be needed (these are just copies/templates); added comments.
    
    * Enable Logout; Add functions.php; Create SetupSMR Form
    
    - Corrected logout issue on all non-index.php pages.
    - Created functions.php to support other necessary development.
    - Created appropriate form content on setupsmr.php & adapted this onto mysettings.php. Note that the accompanying process files still need to be written.
    
    * Correct autoloader.php
    
    * Remove duplicate function wh_log
    
    * Realign with master
    
    Fixing commits from upstream issues.
    
    * Completed SetupSMR
    
    Completed and unit-tested SetupSMR page with database insert but WITHOUT the CPanel API calls originally in scope. These were descoped for the CIS-492 Capstone team. This includes supporting changes to functions.php and process_setupsmr.php.
    
    Also features changes to index.php to conditionally display confirmation messaging to new users who have or have not completed setup.
    
    * Settings Management Adjustments
    
    -Correct pulling existing user's settings
    -Correct Logout Button Behavior
    -Remove extra Submit Button
    -Restore Ajax/Json functionality for the update button
    -Add validation
    
    ---------
    
    Co-authored-by: xancara <xancara@gmail.com>
    Co-authored-by: phixious <xancara@users.noreply.github.com>

--- php/process_mysettings.php
+++ php/process_mysettings.php
@@ -5,48 +5,65 @@ include_once '../autoloader.php';
 
 $userDetails = getRowWithValue( 'userdetails', 'id', $_SESSION['user_details']['id'] ); //get user details to compare with values posted
 
-$id = $_SESSION['user_details']['id']; //get id
+$id = $_SESSION['user_details']['id']; //get userDetails id from session
 
-$twitchChannel = $_POST['twitch_channel']; //get twitch channel name from post
-if ( $twitchChannel != $userDetails['twitchChannel'] ) {  //if twitch channel from post is not equal to value in database
-	updateRow( 'userDetails', 'twitchChannel', $twitchChannel, $id);   //update that row with new value from post
-}
-
-// Repeat same steps for each of the updatable fields
-
+//Get form data from URL
+$twitchChannel = $_POST['twitch_channel'];
 $smProfile = $_POST['sm_profile'];
-if ( $smProfile != $userDetails['smProfile'] ) {
-	updateRow( 'userDetails', 'smProfile', $smProfile, $id );
-}
-
 $chatbot = $_POST['chatbot'];
-if ( $chatbot != $userDetails['chatbot'] ) {
-	updateRow( 'userDetails', 'chatbot', $chatbot, $id );
-}
-
 $securityKey = $_POST['security_key'];
-if ( $securityKey != $userDetails['securityKey'] ) {
-	updateRow( 'userDetails', 'securityKey', $securityKey, $id );
-}
-
 $maxRequests = $_POST['maxRequests'];
-if ( $maxRequests != $userDetails['maxRequests'] ) {
-	updateRow( 'userDetails', 'maxRequests', $maxRequests, $id );
-}
-
 $cooldownMultiplier = $_POST['cooldownMultiplier'];
-if ( $cooldownMultiplier != $userDetails['cooldownMultiplier'] ) {
-	updateRow( 'userDetails', 'cooldownMultiplier', $cooldownMultiplier, $id );
-}
-
 $scoreType = $_POST['scoreType'];
-if ( $scoreType != $userDetails['scoreType'] ) {
-	updateRow( 'userDetails', 'scoreType', $scoreType, $id );
-}
-
 $topPercent = $_POST['topPercent'];
-if ( $topPercent != $userDetails['topPercent'] ) {
-	updateRow( 'userDetails', 'topPercent', $topPercent, $id );
+
+//Validate field entries for submission.
+if (empty($twitchChannel) || strlen($twitchChannel) > 25) { // ensure channel neither empty nor too long
+	$status = 'fail';
+	$message = 'A valid Twitch Channel was not specified.';
+} elseif ( empty($smProfile) || strlen($smProfile) > 25) { // ensure StepMania profile isn't empty or too long
+	$status = 'fail';
+	$message = 'A valid StepMania Profile Name was not specified.';
+} elseif ( empty( $securityKey) ) { // ensure Security key is not blank
+	$status = 'fail';
+	$message = 'You must specify a security key.';
+} elseif ( empty($maxRequests) || $maxRequests < 1 || $maxRequests > 10) { // max requests validation
+	$status = 'fail';
+	$message = 'Please specify a valid Maximum Requests value, 1-10';
+} elseif ( empty($cooldownMultiplier) || $cooldownMultiplier < 0.01 || $cooldownMultiplier > 1) { // max requests validation
+	$status = 'fail';
+	$message = 'Please specify a valid Cooldown Multiplier, 0.01-1';
+} elseif ( empty($topPercent) || $topPercent < 0.01 || $topPercent > 1) { // max requests validation
+	$status = 'fail';
+	$message = 'Please specify a valid Top Percent, 0.01-1';
+} else { // all passes so we are all good!
+	$status = 'ok';
+	$message = 'valid';
+	//process updates of updated fields
+	if ( $twitchChannel != $userDetails['twitchChannel'] ) {  //if twitch channel from post is not equal to value in database
+		updateRow( 'userDetails', 'twitchChannel', $twitchChannel, $id);   //update that row with new value from post
+	}
+	if ( $smProfile != $userDetails['smProfile'] ) {
+		updateRow( 'userDetails', 'smProfile', $smProfile, $id );
+	}
+	if ( $chatbot != $userDetails['chatbot'] ) {
+		updateRow( 'userDetails', 'chatbot', $chatbot, $id );
+	}
+	if ( $securityKey != $userDetails['securityKey'] ) {
+		updateRow( 'userDetails', 'securityKey', $securityKey, $id );
+	}
+	if ( $maxRequests != $userDetails['maxRequests'] ) {
+		updateRow( 'userDetails', 'maxRequests', $maxRequests, $id );
+	}
+	if ( $cooldownMultiplier != $userDetails['cooldownMultiplier'] ) {
+		updateRow( 'userDetails', 'cooldownMultiplier', $cooldownMultiplier, $id );
+	}
+	if ( $scoreType != $userDetails['scoreType'] ) {
+		updateRow( 'userDetails', 'scoreType', $scoreType, $id );
+	}
+	if ( $topPercent != $userDetails['topPercent'] ) {
+		updateRow( 'userDetails', 'topPercent', $topPercent, $id );
+	}
 }
 
 // get user details so we have most recent info
@@ -55,15 +72,10 @@ $userDetails = getRowWithValue( 'userdetails', 'id', $_SESSION['user_details']['
 //update session with updated user details
 $_SESSION['user_details'] = $userDetails;
 
-// redirect to mysettings.php
-header( 'location: ../mysettings.php' );
 
-
-/*
 	echo json_encode( // return json for ajax on front end
 		array(
 			'status' => $status,
 			'message' => $message
 		)
 	);
-*/
